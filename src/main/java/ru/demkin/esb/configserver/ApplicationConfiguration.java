6dd44b7a0413a7a40c1dd25c96e42c44a4c569d1
Eugene <evgen1000end@gmail.com>
2020-04-30 11:57:19 +0300

    Миграция на swagger v3 OpenAPI

--- src/main/java/ru/demkin/esb/configserver/ApplicationConfiguration.java
+++ src/main/java/ru/demkin/esb/configserver/ApplicationConfiguration.java
@@ -1,24 +1,22 @@
 package ru.demkin.esb.configserver;
 
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class ApplicationConfiguration {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI() //
+                .components(new Components()) //
+                .info(new Info() //
+                        .title("Адаптер УФЭС") //
+                        .description("RESTful API для отправки в ESB УФЭС сообщений") //
+                        .version(getClass().getPackage().getImplementationVersion()));
     }
 }
--- src/main/java/ru/demkin/esb/configserver/controllers/ConfigController.java
+++ src/main/java/ru/demkin/esb/configserver/controllers/ConfigController.java
@@ -1,7 +1,7 @@
 package ru.demkin.esb.configserver.controllers;
 
 
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +17,13 @@ public class ConfigController {
     @Autowired
     private ConfigService service;
 
-    @ApiOperation("Записать тестовое сообщение")
+    @Operation(summary = "Записать тестовое сообщение")
     @PostMapping("/save")
     public void save(@RequestBody String value) {
         service.save(value);
     }
 
-    @ApiOperation("Прочитать все сообщения")
+    @Operation(summary = "Прочитать все сообщения")
     @GetMapping("/load")
     public List<String> load() {
         return service.load();
